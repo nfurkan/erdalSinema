4eed60f1c7efad693a6123e0028380a583d18365
furkanf <fur-001@hotmail.com>
2022-12-29 02:31:28 +0300

    update

--- src/main/java/com/erdal/clsMovie.java
+++ src/main/java/com/erdal/clsMovie.java
@@ -141,7 +141,26 @@ public class clsMovie {
     public void addMovie(){
     
     
+    }
     
+    public void removeBroadcast(){
+
+    
+    
+    }
+    
+    public void addBroadcast(){
+       
+    String sql = "UPDATE `grup11`.`tbMovies` SET `broadcasting` = '1' WHERE `tbMovies`.`ID` =16;";    
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup11?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup11", "erdal");
+            Statement stmt = con.createStatement();
+            stmt.execute(sql);
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
     
     @Override
