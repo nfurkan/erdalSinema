3383ac43da7f8abd3df93a39bcf8be75c9998f43
furkanf <fur-001@hotmail.com>
2023-01-02 07:42:10 +0300

    update

--- src/main/java/com/erdal/clsMovieSessions.java
+++ src/main/java/com/erdal/clsMovieSessions.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 /**
@@ -96,7 +97,7 @@ public class clsMovieSessions {
         return movieSessions;
     }
     
-    public String addMovieSession(Date startTime, Date finishTime, int saloon, int movieId, int totalSeatNumber){
+    public String addMovieSession(LocalDateTime startTime, LocalDateTime finishTime, int saloon, int movieId, int totalSeatNumber){
         String sql = "INSERT INTO `grup11`.`tbMovieSessions` (`ID`, `starttime`, `finishtime`, `saloon`, `movieID`, `totalseatnumber`) "
                 + "VALUES (NULL, '2023-01-11 09:30:00', '2023-01-11 12:30:00', '4', '17', '40');";
             
@@ -104,8 +105,12 @@ public class clsMovieSessions {
                 Class.forName("com.mysql.jdbc.Driver");
                 Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup11?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup11", "erdal");
                 PreparedStatement stmt = con.prepareStatement("INSERT INTO tbMovieSessions (starttime, finishtime, saloon, movieID, totalseatnumber) VALUES (?,?,?,?,?)");
-                stmt.setDate(1,startTime);
-                stmt.setDate(2,finishTime);
+                LocalDateTime dateTime = startTime;
+                java.sql.Date sqlDate = java.sql.Date.valueOf(dateTime.toLocalDate());
+                LocalDateTime dateTime1 = finishTime;
+                java.sql.Date sqlDate1 = java.sql.Date.valueOf(dateTime1.toLocalDate());
+                stmt.setDate(1,sqlDate);
+                stmt.setDate(2,sqlDate1);
                 stmt.setInt(3,saloon);
                 stmt.setInt(4,movieId);
                 stmt.setInt(5,totalSeatNumber);
--- target/webProject-1.0-SNAPSHOT/seansRedirect.jsp
+++ target/webProject-1.0-SNAPSHOT/seansRedirect.jsp
@@ -4,6 +4,7 @@
 <%@page import="java.sql.*"%>
 <%@page import="java.util.ArrayList"%>
 <%@page import="java.util.Date"%>
+<%@page import="java.time.LocalDateTime"%>
 <jsp:useBean id="user" class="com.erdal.clsUser" scope="session"/>
 <jsp:useBean id="cart" class="com.erdal.clsCart" scope="session"/>
 <jsp:useBean id="movieSession" class="com.erdal.clsMovieSessions" scope="session"/>
@@ -15,7 +16,13 @@
     <body>
         <%
             
-            
+            clsMovieSessions seans = new clsMovieSessions();
+            LocalDateTime startTime = LocalDateTime.parse(request.getParameter("startTime"));
+            LocalDateTime finishTime = LocalDateTime.parse(request.getParameter("finishTime"));
+            int saloon, movieId, totalSeatNumber;
+            saloon = Integer.parseInt(request.getParameter("saloon"));
+            totalSeatNumber = Integer.parseInt(request.getParameter("totalSeatNumber"));
+            seans.addMovieSession(startTime, finishTime, saloon, 4, totalSeatNumber);
 
         %>
     </body>
